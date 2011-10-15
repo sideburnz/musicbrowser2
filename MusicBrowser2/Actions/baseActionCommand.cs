d3849b530a8af464ae45f4e2f30a7e6f3333a816
justin scott joyce <justin.scott.joyce@gmail.com>
2011-10-15 17:44:14 +0100

    2.2.3.1 - 2011-10-13
    - work to introduce Actions

---
+++ MusicBrowser2/Actions/baseActionCommand.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MusicBrowser.Entities;
+using MusicBrowser.Providers;
+using MusicBrowser.Models;
+using Microsoft.MediaCenter.UI;
+
+namespace MusicBrowser.Actions
+{
+    public abstract class baseActionCommand : BaseModel
+    {
+        public baseActionCommand()
+        {
+            IconPath = "resx://MusicBrowser/MusicBrowser.Resources/nullImage";
+            Label = this.GetType().Name;
+        }
+
+        public string Label { get; set; }
+
+        public void Execute(Entity entity, String caller)
+        {
+            string title;
+            if (entity == null)
+            {
+                title = "none defined";
+            }
+            else
+            {
+                title = entity.Title;
+            }
+
+            Logging.Logger.Verbose(String.Format("Action: {0}, Entity: {1}", Description, title), "start");
+
+            Statistics.Hit("Action." + Description + "." + caller);
+
+            try
+            {
+                DoAction(entity);
+            }
+            catch(Exception e)
+            {
+                Logging.Logger.Error(e);
+            }
+
+            Logging.Logger.Verbose(String.Format("Action: {0}, Entity: {1}", Description, title), "finish");
+        }
+
+        public virtual void DoAction(Entity entity) {}
+
+        public string IconPath { get; set; }
+
+        public Image Icon
+        {
+            get { return new Image(IconPath); }
+        }
+    }
+}
---
+++ MusicBrowser2/Models/ActionsModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.MediaCenter.UI;
+using MusicBrowser.Actions;
+using MusicBrowser.Entities;
+
+namespace MusicBrowser.Models
+{
+    public class ActionsModel : BaseModel
+    {
+        //TODO: this should also do the work for the Actions - marshal the Execute calls
+
+        #region singleton
+        static ActionsModel _instance;
+        
+        public static ActionsModel GetInstance
+        {
+            get 
+            {
+            if (_instance != null) return _instance;
+            _instance = new ActionsModel();
+            return _instance;
+            }
+        }
+        #endregion
+
+
+        public bool _visible = false;
+
+        public bool Visible
+        {
+            get 
+            { 
+                return _visible; 
+            }
+            set
+            {
+                if (_visible != value)
+                {
+                    _visible = value;
+                    FirePropertyChanged("Visible");
+                }
+            }
+        }
+
+        public void ExecuteAction(baseActionCommand action, Entity entity, String context )
+        {
+            try
+            {
+                action.Execute(entity, context);
+            }
+            catch
+            {
+            }
+        }
+
+    }
+
+}
