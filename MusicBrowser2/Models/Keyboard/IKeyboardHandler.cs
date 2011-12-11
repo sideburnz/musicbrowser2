f92b3d89c34f0e09246c3380bd0f51dd9014a346
justin scott joyce <justin.scott.joyce@gmail.com>
2011-12-11 15:36:16 +0000

    2.2.4.0 2011-12-11

---
+++ MusicBrowser2/Models/Keyboard/IKeyboardHandler.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.MediaCenter.UI;
+using MusicBrowser.Entities;
+
+namespace MusicBrowser.Models.Keyboard
+{
+    // for this to work the page needs to feed the data into the dataset here and then from here into the list shown on the scren
+    public abstract class IKeyboardHandler
+    {
+        private int _index = 0;
+        private EntityCollection _rawdata;
+        private EntityCollection _processeddata;
+        private string _value = String.Empty;
+
+        public int Index 
+        { 
+            get
+            {
+                return _index;
+            }
+            set
+            {
+                _index = value;
+                if (!(OnDataChanged == null))
+                {
+                    OnDataChanged("Index");
+                }
+            }
+        }
+
+        public EntityCollection RawDataSet
+        {
+            set
+            {
+                _rawdata = value;
+                _processeddata = value;
+                if (!(OnDataChanged == null))
+                {
+                    OnDataChanged("DataSet");
+                }
+            }
+            protected get
+            {
+                return _rawdata;
+            }
+        }
+
+        public EntityCollection DataSet
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(Value))
+                {
+                    return _rawdata;
+                }
+                return _processeddata;
+            }
+            protected set
+            {
+                _processeddata = value;
+                _rawdata = value;
+                if (!(OnDataChanged == null))
+                {
+                    OnDataChanged("DataSet");
+                }
+            }
+        }
+
+        public string Value 
+        { 
+            get
+            {
+                return _value;
+            } 
+            set
+            {
+                _value = value;
+                DoService();
+                if (!(OnDataChanged == null))
+                {
+                    OnDataChanged("Value");
+                }
+            }
+        }
+
+        public abstract void DoService();
+
+        public delegate void DataChangedHandler(String key);
+        public static event DataChangedHandler OnDataChanged;
+    }
+}
---
+++ MusicBrowser2/Models/Keyboard/KeyboardSearch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MusicBrowser.Actions;
+
+namespace MusicBrowser.Models.Keyboard
+{
+    class KeyboardSearch : IKeyboardHandler
+    {
+        public override void DoService()
+        {
+            if (String.IsNullOrEmpty(Value))
+            {
+                return;
+            }
+            ActionShowSearch action = new ActionShowSearch();
+            action.SearchString = Value;
+            action.Invoke();
+            Value = String.Empty;
+        }
+    }
+}
