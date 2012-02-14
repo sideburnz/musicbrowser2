6f4056d9ddb7084677ac9a9e89c6b7264ed0131b
justin scott joyce <justin.scott.joyce@gmail.com>
2012-02-14 23:01:37 +0000

    2.2.4.0 2011-02-14

--- MusicBrowser2/Engines/Cache/SQLiteHelper.cs
+++ MusicBrowser2/Engines/Cache/SQLiteHelper.cs
@@ -10,39 +10,9 @@ namespace MusicBrowser.Engines.Cache
 {
     static class SQLiteHelper
     {
-        private static bool LoadDLL = Loaded();
         private static readonly Dictionary<string, SQLiteConnection> _connections = new Dictionary<string, SQLiteConnection>();
 
-        private static bool Is64Bit
-        {
-            get { return IntPtr.Size == 8; }
-        }
 
-        private static System.Reflection.Assembly sqliteAssembly;
-        private static System.Reflection.Assembly SqliteResolver(object sender, ResolveEventArgs args)
-        {
-            if (args.Name.StartsWith("System.Data.SQLite,"))
-            {
-                return sqliteAssembly;
-            }
-            return null;
-        }
-
-        private static bool Loaded()
-        {
-            string dllfile = Path.Combine(Helper.ComponentFolder, String.Format("System.Data.SQLite.DLL"));
-            if (!File.Exists(dllfile))
-            {
-                string sourceFile = Path.Combine(Helper.ComponentFolder, String.Format("System.Data.SQLite.DLL.{0}", Is64Bit ? 64 : 32));
-                if (File.Exists(sourceFile))
-                {
-                    File.Copy(sourceFile, dllfile);
-                    sqliteAssembly = System.Reflection.Assembly.LoadFile(dllfile);
-                    AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(SqliteResolver);
-                }
-            }
-            return true;
-        }
 
         public static bool EstablishDatabase(string file, string createSQL)
         {
