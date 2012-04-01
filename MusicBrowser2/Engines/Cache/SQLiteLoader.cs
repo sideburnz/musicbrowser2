6eb168b3faef1face3614ddf10b5856f518ff44c
justin scott joyce <justin.scott.joyce@gmail.com>
2012-04-01 20:29:51 +0100

    - fixed bug causing unresponsiveness using default player engine - application should now run on Windows 8

--- MusicBrowser2/Engines/Cache/SQLiteLoader.cs
+++ MusicBrowser2/Engines/Cache/SQLiteLoader.cs
@@ -1,19 +1,18 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
 using MusicBrowser.Util;
 
 namespace MusicBrowser.Engines.Cache
 {
     class SQLiteLoader
     {
-        private static bool _loadDll = Loaded();
-
-        public static System.Reflection.Assembly sqliteAssembly;
-        public static System.Reflection.Assembly SqliteResolver(object sender, ResolveEventArgs args)
+        public static Assembly SqliteAssembly;
+        public static Assembly SqliteResolver(object sender, ResolveEventArgs args)
         {
             if (args.Name.StartsWith("System.Data.SQLite,"))
             {
-                return sqliteAssembly;
+                return SqliteAssembly;
             }
             return null;
         }
@@ -23,24 +22,26 @@ namespace MusicBrowser.Engines.Cache
             get { return IntPtr.Size == 8; }
         }
 
-        private static bool Loaded()
-        {
-            string dllfile = Path.Combine(Helper.ComponentFolder, String.Format("System.Data.SQLite.DLL"));
-            if (!File.Exists(dllfile))
-            {
-                string sourceFile = Path.Combine(Helper.ComponentFolder, String.Format("System.Data.SQLite.DLL.{0}", Is64Bit ? 64 : 32));
-                if (File.Exists(sourceFile))
-                {
-                    File.Copy(sourceFile, dllfile);
-                }
-            }
-            sqliteAssembly = System.Reflection.Assembly.LoadFile(dllfile);
-            AppDomain.CurrentDomain.AssemblyResolve += SqliteResolver;
-            return true;
-        }
-
         public static void Load()
         {
+            try
+            {
+                string dllfile = Path.Combine("C:\\Windows\\eHome", String.Format("System.Data.SQLite.DLL"));
+                if (!File.Exists(dllfile))
+                {
+                    string sourceFile = Path.Combine(Helper.ComponentFolder, String.Format("System.Data.SQLite.DLL.{0}", Is64Bit ? 64 : 32));
+                    if (File.Exists(sourceFile))
+                    {
+                        File.Copy(sourceFile, dllfile);
+                    }
+                }
+                SqliteAssembly = Assembly.Load(dllfile);
+                AppDomain.CurrentDomain.AssemblyResolve += SqliteResolver;
+            }
+            catch (Exception ex)
+            {
+                Logging.LoggerEngineFactory.Error(ex);
+            }
         }
     }
 }
