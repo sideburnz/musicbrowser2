22b9e305bc8fe5cfc2632a1f82b794aff99c2859
justin <justin@lenny.joocer.net>
2012-07-31 23:28:41 +0100

    32 and 64 bit installers

--- Setup/Build.32.bat
+++ Setup/Build.32.bat
@@ -20,7 +20,7 @@ CD G:\Code\MusicBrowser2\setup
 
 SET WIX_BUILD_LOCATION=%ProgramFilesPath%\Windows Installer XML v3.5\bin
 SET APP_INTERMEDIATE_PATH=..\MusicBrowser2\obj\%BUILD_TYPE%
-SET OUTPUTNAME=..\setup.msi
+SET OUTPUTNAME=..\MusicBrowser2 (32 bit).msi
 
 REM Cleanup leftover intermediate files
 
@@ -29,8 +29,8 @@ del /f /q "%OUTPUTNAME%"
 
 REM Build the MSI for the setup package
 
-"%WIX_BUILD_LOCATION%\candle.exe" .\setup.wxs -dBuildType=%BUILD_TYPE% -out "%APP_INTERMEDIATE_PATH%\setup.wixobj"
-"%WIX_BUILD_LOCATION%\light.exe" "%APP_INTERMEDIATE_PATH%\setup.wixobj" -cultures:en-US -ext "%ProgramFilesPath%\Windows Installer XML v3.5\bin\WixUIExtension.dll" -ext "%ProgramFilesPath%\Windows Installer XML v3.5\bin\WixUtilExtension.dll" -loc setup-en-us.wxl -out "%OUTPUTNAME%"
+"%WIX_BUILD_LOCATION%\candle.exe" .\setup.32.wxs -dBuildType=%BUILD_TYPE% -out "%APP_INTERMEDIATE_PATH%\setup.32.wixobj"
+"%WIX_BUILD_LOCATION%\light.exe" "%APP_INTERMEDIATE_PATH%\setup.32.wixobj" -cultures:en-US -ext "%ProgramFilesPath%\Windows Installer XML v3.5\bin\WixUIExtension.dll" -ext "%ProgramFilesPath%\Windows Installer XML v3.5\bin\WixUtilExtension.dll" -loc setup-en-us.wxl -out "%OUTPUTNAME%"
 
 popd
 
